a43267b2122e5b542ed062506810e3b4467b383a
mohamed <32775877+mohamedkhattab2@users.noreply.github.com>
2022-06-04 15:24:20 +0200

    add subclass Salaried employee extends from Employee

--- src/com/inheritance/Main.java
+++ src/com/inheritance/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Employee salariedEmployee= new SalariedEmployee("yass","auun",
+                "111-444-566",500.0);
+        
+        System.out.printf("%s",salariedEmployee.toString());
     }
 }
---
+++ src/com/inheritance/SalariedEmployee.java
@@ -0,0 +1,44 @@
+package com.inheritance;
+
+// salaried employee concrete class extends class Employee
+public class SalariedEmployee extends Employee {
+
+    // instance variables
+    private double weeklySalary;
+
+    // constructor
+    public SalariedEmployee
+    (String firstName, String lastName, String socialSecurityNumber, double weeklySalary) {
+        super(firstName, lastName, socialSecurityNumber);
+
+        if (weeklySalary < 0.0) {
+            throw new IllegalArgumentException
+                    ("weekly salary must be >=0");
+        }
+        this.weeklySalary = weeklySalary;
+    }
+
+
+    public double getWeeklySalary() {
+        return weeklySalary;
+    }
+
+    public void setWeeklySalary(double weeklySalary) {
+        if (weeklySalary < 0.0) {
+            throw new IllegalArgumentException
+                    ("weekly salary must be >=0");
+        }
+        this.weeklySalary = weeklySalary;
+    }
+
+    @Override
+    public double earnings() {
+        return getWeeklySalary();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s%n%s: $%.2f", "salaried employee", super.toString(), "weekly salary",
+                getWeeklySalary());
+    }
+} // end class Salaried employee
